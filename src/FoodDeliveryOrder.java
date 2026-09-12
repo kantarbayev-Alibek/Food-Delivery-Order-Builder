4ad427e1c804f90bcce7e54290c706a508dae465
Alibek <kantarbaieva@list.ru>
2026-09-12 19:10:58 +0500

    2nd commit

--- src/FoodDeliveryOrder.java
+++ src/FoodDeliveryOrder.java
@@ -4,6 +4,14 @@ public class FoodDeliveryOrder {
     private String drink;
     private String deliveryAddress;
     private boolean delivery;
+    public FoodDeliveryOrder(String restaurant, String mainDish, String drink,
+                             String deliveryAddress, boolean delivery) {
+        this.restaurant = restaurant;
+        this.mainDish = mainDish;
+        this.drink = drink;
+        this.deliveryAddress = deliveryAddress;
+        this.delivery = delivery;
+    }
     public void showOrder() {
         System.out.println("restaurant: " + restaurant);
         System.out.println("main dish: " + mainDish);
---
+++ src/FoodDeliveryOrderBuilder.java
@@ -0,0 +1,36 @@
+public class FoodDeliveryOrderBuilder {
+    private String restaurant;
+    private String mainDish;
+    private String drink;
+    private String deliveryAddress;
+    private boolean delivery;
+    public FoodDeliveryOrderBuilder setRestaurant(String restaurant) {
+        this.restaurant = restaurant;
+        return this;
+    }
+    public FoodDeliveryOrderBuilder  setMainDish(String mainDish) {
+        this.mainDish = mainDish;
+        return this;
+    }
+    public FoodDeliveryOrderBuilder setDrink(String drink) {
+        this.drink = drink;
+        return this;
+    }
+    public FoodDeliveryOrderBuilder setDeliveryAddress(String deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+        return this;
+    }
+    public FoodDeliveryOrderBuilder setDelivery(boolean delivery){
+        this.delivery = delivery;
+        return this;
+    }
+    public FoodDeliveryOrder build() {
+        return new FoodDeliveryOrder(
+                restaurant,
+                mainDish,
+                drink,
+                deliveryAddress,
+                delivery
+        );
+    }
+}
